ebba2046bd5b80ca7b0d60584b482322b1f04e66
yousef <nayo21da@student.ju.se>
2023-08-09 20:31:54 +0200

    jkndks

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const inputbox = document.getElementById("input-box");
-const listcontainer = document.getElementById("list-container");
-function addTaask() {
-  if (inputbox.value === "") {
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+//got helped by YouTube tutorial
+
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+function addTask() {
+  if (inputBox.value === "") {
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputbox.value;
-    listcontainer.appendChild(li);
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData();
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function showList() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+showList();
